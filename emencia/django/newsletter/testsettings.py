2b7bacfe619f75e6a8763c1046aadf68967feffe
Fantomas42 <fantomas42@gmail.com>
2009-10-13 12:41:39 +0200

    adding tests

---
+++ emencia/django/newsletter/testsettings.py
@@ -0,0 +1,24 @@
+import os
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = '/tmp/links.db'
+INSTALLED_APPS = ['django.contrib.auth',
+                  'django.contrib.contenttypes',
+                  'django.contrib.sessions',
+                  'emencia.django.newsletter',]
+ROOT_URLCONF = 'emencia.django.newsletter.urls'
+
+LANGUAGE_CODE = 'fr'
+
+LANGUAGES = (
+    ('fr', 'French'),
+    ('en', 'English'),
+    )
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    )
