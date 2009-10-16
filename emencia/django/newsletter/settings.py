7b4d40bb9a46386c2034a9ab8c25f6db5955a1c4
Fantomas42 <fantomas42@gmail.com>
2009-10-16 10:11:22 +0200

    correcting settings

--- emencia/django/newsletter/settings.py
+++ emencia/django/newsletter/settings.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 INCLUDE_UNSUBSCRIPTION = getattr(settings, 'NEWSLETTER_INCLUDE_UNSUBSCRIPTION', True)
 
-DEFAULT_HEADER_REPLY = getattr(settings. 'NEWSLETTER_DEFAULT_HEADER_REPLY', 'Emencia Newsletter<noreply@emencia.com>')
-DEFAULT_HEADER_SENDER = getattr(settings. 'NEWSLETTER_DEFAULT_HEADER_SENDER', 'Emencia Newsletter<noreply@emencia.com>')
+DEFAULT_HEADER_REPLY = getattr(settings, 'NEWSLETTER_DEFAULT_HEADER_REPLY', 'Emencia Newsletter<noreply@emencia.com>')
+DEFAULT_HEADER_SENDER = getattr(settings, 'NEWSLETTER_DEFAULT_HEADER_SENDER', 'Emencia Newsletter<noreply@emencia.com>')
 
