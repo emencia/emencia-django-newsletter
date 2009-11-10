fb096868fc42ff41705d3d9ecfd95e32c426402f
Fantomas42 <fantomas42@gmail.com>
2009-11-10 12:20:18 +0100

    adding domain for links

--- emencia/django/newsletter/utils.py
+++ emencia/django/newsletter/utils.py
@@ -38,8 +38,8 @@ def track_links(content, context):
             link_title = link_markup.get('title', link_href)
             link, created = Link.objects.get_or_create(url=link_href,
                                                        defaults={'title': link_title})
-            link_markup['href'] = reverse('newsletter_newsletter_tracking_link',
-                                          args=[context['newsletter'].slug,
-                                                context['uidb36'], context['token'],
-                                                link.pk])
+            link_markup['href'] = 'http://%s%s' % (context['domain'], reverse('newsletter_newsletter_tracking_link',
+                                                                              args=[context['newsletter'].slug,
+                                                                                    context['uidb36'], context['token'],
+                                                                                    link.pk]))
     return soup
