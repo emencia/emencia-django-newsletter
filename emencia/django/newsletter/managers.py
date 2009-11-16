53502334b277e5df5804e4776044e6385f128cce
Fantomas42 <fantomas42@gmail.com>
2009-11-16 15:19:38 +0100

    adding contact manager

---
+++ emencia/django/newsletter/managers.py
@@ -0,0 +1,22 @@
+"""Managers for emencia.django.newsletter"""
+from django.db import models
+
+class ContactManager(models.Manager):
+    """Manager for the contacts"""
+
+    def subscribers(self):
+        """Return all subscribers"""
+        return self.get_query_set().filter(subscriber=True)
+
+    def unsubscribers(self):
+        """Return all unsubscribers"""
+        return self.get_query_set().filter(subscriber=False)
+
+    def valids(self):
+        """Return only valid contacts"""
+        return self.get_query_set().filter(valid=True)
+
+    def valid_subscribers(self):
+        """Return only valid subscribers"""
+        return self.subscribers().filter(valid=True)
+
