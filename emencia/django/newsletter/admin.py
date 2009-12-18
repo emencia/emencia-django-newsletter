03b961550b23be076404071faef9fe6544a68b5a
Fantomas42 <fantomas42@gmail.com>
2009-12-18 12:12:17 +0100

    upgrading admin for exporting contacts and cleaning docstring

--- emencia/django/newsletter/admin.py
+++ emencia/django/newsletter/admin.py
@@ -1,8 +1,9 @@
 """Admin for emencia.django.newsletter"""
-from HTMLParser import HTMLParseError
 from datetime import datetime
+from HTMLParser import HTMLParseError
 
 from django.contrib import admin
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
@@ -19,6 +20,7 @@ from emencia.django.newsletter.models import MailingList
 from emencia.django.newsletter.models import Newsletter
 from emencia.django.newsletter.models import ContactMailingStatus
 from emencia.django.newsletter.utils import get_webpage_content
+from emencia.django.newsletter.vcard import vcard_contacts_export_response
 
 class SMTPServerAdmin(admin.ModelAdmin):
     list_display = ('name', 'host', 'port', 'user', 'tls', 'mails_hour',)# 'check_connection')
@@ -35,11 +37,12 @@ class SMTPServerAdmin(admin.ModelAdmin):
     actions_on_bottom = True
 
     def check_connections(self, request, queryset):
+        """Check the SMTP connection"""
         message = '%s connection %s'
-        for server in queryset:            
+        for server in queryset:
             status = server.connection_valid() and 'OK' or 'KO'
             self.message_user(request, message % (server.__unicode__(), status))
-    
+
 admin.site.register(SMTPServer, SMTPServerAdmin)
 
 class ContactAdmin(admin.ModelAdmin):
@@ -54,11 +57,12 @@ class ContactAdmin(admin.ModelAdmin):
                  (_('Advanced'), {'fields': ('object_id', 'content_type'),
                                   'classes': ('collapse',)}),
                  )
-    actions = ['create_mailinglist',]
+    actions = ['create_mailinglist', 'export_vcard']
     actions_on_top = False
     actions_on_bottom = True
 
     def related_object_admin(self, contact):
+        """Display link to related object's admin"""
         if contact.content_type and contact.object_id:
             admin_url = reverse('admin:%s_%s_change' % (contact.content_type.app_label,
                                                         contact.content_type.model),
@@ -71,22 +75,46 @@ class ContactAdmin(admin.ModelAdmin):
     related_object_admin.short_description = _('Related object')
 
     def total_subscriptions(self, contact):
+        """Display user subscriptions to unsubscriptions"""
         subscriptions = contact.subscriptions().count()
         unsubscriptions = contact.unsubscriptions().count()
         return '%s / %s' % (subscriptions - unsubscriptions, subscriptions)
     total_subscriptions.short_description = _('Total subscriptions')
 
+    def export_vcard(self, request, queryset, export_name=''):
+        """Export selected contact in VCard"""
+        return vcard_contacts_export_response(queryset)
+    export_vcard.short_description = _('Export contacts as VCard')
+
     def create_mailinglist(self, request, queryset):
+        """Create a mailing list from selected contact"""
         when = str(datetime.now()).split('.')[0]
-        new_mailing = MailingList(name=_('New mailinglist at %s') % when, 
+        new_mailing = MailingList(name=_('New mailinglist at %s') % when,
                                   description=_('New mailing list created in admin at %s') % when)
         new_mailing.save()
         new_mailing.subscribers = queryset.all()
-        
+
         self.message_user(request, _('%s succesfully created.') % new_mailing)
-        
     create_mailinglist.short_description = _('Create a mailinglist')
-        
+
+    def importation(self, request):
+        """Import contacts from a VCard"""
+        #TODO
+        pass
+
+    def exportation(self, request):
+        """Export all the contact in VCard"""
+        return self.export_vcard(request, Contact.objects.all(),
+                                 'all_contacts_edn_%s' % datetime.now().strftime('%d-%m-%Y'))
+
+    def get_urls(self):
+        urls = super(ContactAdmin, self).get_urls()
+        my_urls = patterns('',
+                           url(r'^import/$', self.importation,
+                               name='newsletter_contact_import'),
+                           url(r'^export/$', self.exportation,
+                               name='newsletter_contact_export'),)
+        return my_urls + urls
 
 admin.site.register(Contact, ContactAdmin)
 
@@ -94,7 +122,7 @@ class MailingListAdmin(admin.ModelAdmin):
     date_hierarchy = 'creation_date'
     list_display = ('name', 'description',
                     'subscribers_count', 'unsubscribers_count',
-                    'creation_date', 'modification_date')    
+                    'creation_date', 'exportation_link')
     list_filter = ('creation_date', 'modification_date')
     search_fields = ('name', 'description',)
     filter_horizontal = ['subscribers', 'unsubscribers']
@@ -107,25 +135,44 @@ class MailingListAdmin(admin.ModelAdmin):
     actions_on_bottom = True
 
     def merge_mailinglist(self, request, queryset):
+        """Merge multiple mailing list"""
         if queryset.count() == 1:
             self.message_user(request, _('Please select a least 2 mailing list.'))
             return None
-        
+
         contacts = {}
         for ml in queryset:
             for contact in ml.subscribers.all():
                 contacts[contact] = ''
 
         when = str(datetime.now()).split('.')[0]
-        new_mailing = MailingList(name=_('Merging list at %s') % when, 
+        new_mailing = MailingList(name=_('Merging list at %s') % when,
                                   description=_('Mailing list created by merging at %s') % when)
         new_mailing.save()
         new_mailing.subscribers = contacts.keys()
-        
+
         self.message_user(request, _('%s succesfully created by merging.') % new_mailing)
-        
     merge_mailinglist.short_description = _('Merge selected mailinglists')
-    
+
+    def exportation_link(self, mailinglist):
+        """Display link for exportation"""
+        return '<a href="%s">%s</a>' % (reverse('admin:newsletter_mailinglist_export', args=[mailinglist.pk,]),
+                                        _('Export Subscribers'))
+    exportation_link.allow_tags = True
+    exportation_link.short_description = _('Export')
+
+    def export_subscribers(self, request, mailinglist_id):
+        """Export subscribers in the mailing in VCard"""
+        mailinglist = get_object_or_404(MailingList, pk=mailinglist_id)
+        name = 'contacts_%s' % mailinglist.name
+        return vcard_contacts_export_response(mailinglist.subscribers.all(), name)
+
+    def get_urls(self):
+        urls = super(MailingListAdmin, self).get_urls()
+        my_urls = patterns('',
+                           url(r'^export/(?P<mailinglist_id>\d+)/$', self.export_subscribers,
+                               name='newsletter_mailinglist_export'),)
+        return my_urls + urls
 
 admin.site.register(MailingList, MailingListAdmin)
 
@@ -143,7 +190,7 @@ class NewsletterAdmin(admin.ModelAdmin):
                  (_('Sending'), {'fields': ('sending_date', 'status',)}),
                  (_('Miscellaneous'), {'fields': ('server', 'header_sender',
                                                   'header_reply', 'slug'),
-                                       'classes': ('collapse',)}),                 
+                                       'classes': ('collapse',)}),
                  )
     prepopulated_fields = {'slug': ('title',)}
     actions = ['send_mail_test', 'make_ready_to_send', 'make_cancel_sending']
@@ -157,11 +204,6 @@ class NewsletterAdmin(admin.ModelAdmin):
             del actions['make_cancel_sending']
         return actions
 
-    def historic_link(self, newsletter):
-        return '<a href="%s">%s</a>' % (newsletter.get_historic_url(), _('View historic'))
-    historic_link.allow_tags = True
-    historic_link.short_description = _('Historic')
-
     #def formfield_for_choice_field(self, db_field, request, **kwargs):
     #    if db_field.name == 'status' and \
     #           not request.user.has_perm('newsletter.can_change_status'):
@@ -173,7 +215,7 @@ class NewsletterAdmin(admin.ModelAdmin):
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == 'test_contacts':
-            kwargs['queryset'] = Contact.objects.filter(tester=True)        
+            kwargs['queryset'] = Contact.objects.filter(tester=True)
         return super(NewsletterAdmin, self).formfield_for_manytomany(
             db_field, request, **kwargs)
 
@@ -186,10 +228,17 @@ class NewsletterAdmin(admin.ModelAdmin):
 
         if not request.user.has_perm('newsletter.can_change_status'):
             newsletter.status = form.initial.get('status', Newsletter.DRAFT)
-        
+
         newsletter.save()
-        
+
+    def historic_link(self, newsletter):
+        """Display link for historic"""
+        return '<a href="%s">%s</a>' % (newsletter.get_historic_url(), _('View historic'))
+    historic_link.allow_tags = True
+    historic_link.short_description = _('Historic')
+
     def send_mail_test(self, request, queryset):
+        """Send newsletter in test"""
         for newsletter in queryset:
             if newsletter.test_contacts.count():
                 mailer = Mailer(newsletter, test=True)
@@ -204,6 +253,7 @@ class NewsletterAdmin(admin.ModelAdmin):
     send_mail_test.short_description = _('Send test email')
 
     def make_ready_to_send(self, request, queryset):
+        """Make newsletter ready to send"""
         queryset = queryset.filter(status=Newsletter.DRAFT)
         for newsletter in queryset:
             newsletter.status = Newsletter.WAITING
@@ -212,6 +262,7 @@ class NewsletterAdmin(admin.ModelAdmin):
     make_ready_to_send.short_description = _('Make ready to send')
 
     def make_cancel_sending(self, request, queryset):
+        """Cancel the sending of newsletters"""
         queryset = queryset.filter(Q(status=Newsletter.WAITING) |
                                    Q(status=Newsletter.SENDING))
         for newsletter in queryset:
@@ -224,7 +275,7 @@ class NewsletterAdmin(admin.ModelAdmin):
         """Display the historic of a newsletters"""
         opts = self.model._meta
         newsletter = get_object_or_404(Newsletter, slug=slug)
-        
+
         context = {'title': _('Stats %s') % newsletter.__unicode__(),
                    'original': newsletter,
                    'opts': opts,
@@ -239,8 +290,7 @@ class NewsletterAdmin(admin.ModelAdmin):
         urls = super(NewsletterAdmin, self).get_urls()
         my_urls = patterns('',
                            url(r'^historic/(?P<slug>[-\w]+)/$', self.historic,
-                               name='newsletter_newsletter_historic'),
-                           )
+                               name='newsletter_newsletter_historic'),)
         return my_urls + urls
 
 admin.site.register(Newsletter, NewsletterAdmin)
@@ -248,7 +298,7 @@ admin.site.register(Newsletter, NewsletterAdmin)
 class LinkAdmin(admin.ModelAdmin):
     list_display = ('title', 'url', 'creation_date')
 
-#admin.site.register(Link, LinkAdmin)
-
-#admin.site.register(ContactMailingStatus)
+if settings.DEBUG:
+    admin.site.register(Link, LinkAdmin)
+    admin.site.register(ContactMailingStatus)
 
