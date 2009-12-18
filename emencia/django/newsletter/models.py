a6e156a7b8c5216dc929996cc765df93968e83fb
Fantomas42 <fantomas42@gmail.com>
2009-12-18 14:26:04 +0100

    adding better default value for newsletter content

--- emencia/django/newsletter/models.py
+++ emencia/django/newsletter/models.py
@@ -184,7 +184,7 @@ class Newsletter(models.Model):
 
     title = models.CharField(_('title'), max_length=255)
     content = models.TextField(_('content'), help_text=_('Or paste an URL.'),
-                               default='<body>\n\n</body>')
+                               default='<body>\n<!-- %s -->\n</body>' % _('Edit your newsletter here'))
     
     mailing_list = models.ForeignKey(MailingList, verbose_name=_('mailing list'))
     test_contacts = models.ManyToManyField(Contact, verbose_name=_('test contacts'),
