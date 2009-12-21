a7fa2c0bd41d3156f2c6e8d2297e0dfa840cb23f
Fantomas42 <fantomas42@gmail.com>
2009-12-21 17:54:46 +0100

    adding more utils for request_workgroups

--- emencia/django/newsletter/utils.py
+++ emencia/django/newsletter/utils.py
@@ -65,3 +65,15 @@ def request_workgroups_contacts_pk(request):
     for workgroup in request_workgroups(request):
         contacts.extend([c.pk for c in workgroup.contacts.all()])
     return set(contacts)
+
+def request_workgroups_mailinglists_pk(request):
+    mailinglists = []
+    for workgroup in request_workgroups(request):
+        mailinglists.extend([ml.pk for ml in workgroup.mailinglists.all()])
+    return set(mailinglists)
+
+def request_workgroups_newsletters_pk(request):
+    newsletters = []
+    for workgroup in request_workgroups(request):
+        newsletters.extend([n.pk for n in workgroup.newsletters.all()])
+    return set(newsletters)
