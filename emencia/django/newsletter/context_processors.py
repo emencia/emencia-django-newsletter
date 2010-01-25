b7b392efc42b6810d44e0863bc5464c319a30600
Fantomas42 <fantomas42@gmail.com>
2010-01-25 14:58:47 +0100

    adding NEWSLETTER_MEDIA_URL context_processor

---
+++ emencia/django/newsletter/context_processors.py
@@ -0,0 +1,6 @@
+"""Context Processors for emencia.django.newsletter"""
+from emencia.django.newsletter.settings import NEWSLETTER_MEDIA_URL
+
+def media(request):
+    """Adds media-related context variables to the context"""
+    return {'NEWSLETTER_MEDIA_URL': NEWSLETTER_MEDIA_URL}
