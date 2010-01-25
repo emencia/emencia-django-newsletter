391702bf9667d69e8a80c7655587ac8a01406795
Fantomas42 <fantomas42@gmail.com>
2010-01-25 11:06:57 +0100

    adding default urls

--- emencia/django/newsletter/urls/__init__.py
+++ emencia/django/newsletter/urls/__init__.py
@@ -1,1 +1,9 @@
+"""Default urls for the emencia.django.newsletter"""
+from django.conf.urls.defaults import *
 
+urlpatterns = patterns('',                       
+                       (r'^mailing/', include('emencia.django.newsletter.urls.mailing_list')),
+                       (r'^tracking/', include('emencia.django.newsletter.urls.tracking')),
+                       (r'^statistics/', include('emencia.django.newsletter.urls.statistics')),
+                       (r'^', include('emencia.django.newsletter.urls.newsletter')),
+                       )
