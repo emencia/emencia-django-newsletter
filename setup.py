b25fe27f1f3a3a0b1d15ac09ef43178294be000b
Fantomas42 <fantomas42@gmail.com>
2010-03-13 11:44:43 +0100

    adding classifier

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='emencia.django.newsletter',
           'Programming Language :: Python',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'License :: OSI Approved :: BSD License',
+          'Framework :: Django',
           ],
 
       author='Fantomas42',
