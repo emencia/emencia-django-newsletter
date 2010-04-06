be5fdaf52c4cee53ba2820a40102f19cc603cbdd
Fantomas42 <fantomas42@gmail.com>
2010-04-06 11:59:59 +0200

    renaming url tracking image

--- emencia/django/newsletter/urls/tracking.py
+++ emencia/django/newsletter/urls/tracking.py
@@ -2,7 +2,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('emencia.django.newsletter.views.tracking',
-                       url(r'^newsletter/(?P<slug>[-\w]+)/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+                       url(r'^newsletter/(?P<slug>[-\w]+)/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+).png$',
                            'view_newsletter_tracking',
                            name='newsletter_newsletter_tracking'),
                        url(r'^link/(?P<slug>[-\w]+)/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/(?P<link_id>\d+)/$',
