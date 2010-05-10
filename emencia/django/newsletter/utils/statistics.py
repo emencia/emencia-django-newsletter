744f231c52d28aa253a9eed1d80e335e8b31aac9
Jérémy <jeremy@jeremy-laptop.(none)>
2010-05-10 20:57:53 +0800

    changed comments

--- emencia/django/newsletter/utils/statistics.py
+++ emencia/django/newsletter/utils/statistics.py
@@ -69,7 +69,7 @@ def get_newsletter_clicked_link_statistics(status, recipients, openings):
 def get_newsletter_unsubscription_statistics(status, recipients):
     unsubscriptions = status.filter(status=Status.UNSUBSCRIPTION)
 
-    """Patch: multiple unsubsriptions logs could exist before a typo bug was corrected, a 'set' is needed"""
+    #Patch: multiple unsubsriptions logs could exist before a typo bug was corrected, a 'set' is needed
     total_unsubscriptions = len(set(unsubscriptions.values_list('contact', flat=True)))
     total_unsubscriptions_percent = float(total_unsubscriptions) / float(recipients) * 100
 
