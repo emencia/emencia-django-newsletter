dc72a78966107d6834b04ede4858f78997f9cec5
lucalenardi <lucalenardi@atra.it>
2010-05-28 01:08:01 +0800

    Updated view and url to support users mailing-list subscription.

--- emencia/django/newsletter/urls/mailing_list.py
+++ emencia/django/newsletter/urls/mailing_list.py
@@ -5,4 +5,8 @@ urlpatterns = patterns('emencia.django.newsletter.views.mailing_list',
                        url(r'^unsubscribe/(?P<slug>[-\w]+)/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
                            'view_mailinglist_unsubscribe',
                            name='newsletter_mailinglist_unsubscribe'),
+
+                       url(r'^subscribe/',
+                           'view_mailinglist_subscribe',
+                           name='newsletter_mailinglist_subscribe'),
                        )
--- emencia/django/newsletter/views/mailing_list.py
+++ emencia/django/newsletter/views/mailing_list.py
@@ -4,15 +4,16 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 
 from emencia.django.newsletter.utils.tokens import untokenize
-from emencia.django.newsletter.models import Newsletter
+from emencia.django.newsletter.models import Newsletter, Contact
 from emencia.django.newsletter.models import ContactMailingStatus
+from emencia.django.newsletter.forms import SubscriptionForm
 
 
 def view_mailinglist_unsubscribe(request, slug, uidb36, token):
     """Unsubscribe a contact to a mailing list"""
     newsletter = get_object_or_404(Newsletter, slug=slug)
     contact = untokenize(uidb36, token)
-    
+
     already_unsubscribed = contact in newsletter.mailing_list.unsubscribers.all()
 
     if request.POST.get('email') and not already_unsubscribed:
@@ -26,4 +27,35 @@ def view_mailinglist_unsubscribe(request, slug, uidb36, token):
                               {'email': contact.email,
                                'already_unsubscribed': already_unsubscribed},
                               context_instance=RequestContext(request))
+
+def view_mailinglist_subscribe(request, template="newsletter/mailing_list_subscribe.html"):
+    """
+    A simple view that shows a form for subscription
+    on one or more mailing-lists at the same time.
+    """
+
+    subscribed = False
+
+    if request.method == 'POST' and not subscribed:
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            contact, generated = Contact.objects.get_or_create(
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name'],
+                email=form.cleaned_data['email'])
+
+            for mailing_list in form.cleaned_data['mailing_lists']:
+                mailing_list.subscribers.add(contact)
+
+            subscribed = True
+    else:
+        form = SubscriptionForm()
+
+    return render_to_response(
+        template,
+        {'subscribed': subscribed,
+         'form': form },
+        context_instance=RequestContext(request)
+    )
+
 
