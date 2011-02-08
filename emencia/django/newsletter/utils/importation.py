4b9ab5864ed0dff04048d14421faa7982df4208d
Fantomas42 <fantomas42@gmail.com>
2011-02-08 17:49:33 +0100

    imported contact are saved in a mailing list

--- emencia/django/newsletter/utils/importation.py
+++ emencia/django/newsletter/utils/importation.py
@@ -1,13 +1,16 @@
 """Utils for importation of contacts"""
 import csv
+from datetime import datetime
 
 import xlrd
 import vobject
 
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 from emencia.django.newsletter.models import Contact
+from emencia.django.newsletter.models import MailingList
 
 
 COLUMNS = ['email', 'first_name', 'last_name', 'tags']
@@ -33,52 +36,67 @@ def create_contact(contact_dict, workgroups=[]):
     return contact, created
 
 
-def vcard_contacts_import(stream, workgroups=[]):
+def create_contacts(contact_dicts, importer_name, workgroups=[]):
+    """Create all the contacts to import and
+    associated them in a mailing list"""
     inserted = 0
-    vcards = vobject.readComponents(stream)
+    when = str(datetime.now()).split('.')[0]
+    mailing_list = MailingList(
+        name=_('Mailing list created by importation at %s') % when,
+        description=_('Contacts imported by %s.') % importer_name)
+    mailing_list.save()
 
-    for vcard in vcards:
-        defaults = {'email': vcard.email.value,
-                    'first_name': vcard.n.value.given,
-                    'last_name': vcard.n.value.family}
-        contact, created = create_contact(defaults, workgroups)
+    for contact_dict in contact_dicts:
+        contact, created = create_contact(contact_dict, workgroups)
+        mailing_list.subscribers.add(contact)
         inserted += int(created)
 
     return inserted
+
+
+def vcard_contacts_import(stream, workgroups=[]):
+    contacts = []
+    vcards = vobject.readComponents(stream)
+
+    for vcard in vcards:
+        contact = {'email': vcard.email.value,
+                   'first_name': vcard.n.value.given,
+                   'last_name': vcard.n.value.family}
+        contacts.append(contact)
+
+    return create_contacts(contacts, 'vcard', workgroups)
 
 
 def text_contacts_import(stream, workgroups=[]):
     """Import contact from a plaintext file, like CSV"""
-    inserted = 0
+    contacts = []
     contact_reader = csv.reader(stream, dialect='edn')
 
     for contact_row in contact_reader:
-        defaults = {}
+        contact = {}
         for i in range(len(contact_row)):
-            defaults[COLUMNS[i]] = contact_row[i]
-        contact, created = create_contact(defaults, workgroups)
-        inserted += int(created)
+            contact[COLUMNS[i]] = contact_row[i]
+        contacts.append(contact)
 
-    return inserted
+    return create_contacts(contacts, 'text', workgroups)
 
 
 def excel_contacts_import(stream, workgroups=[]):
-    inserted = 0
+    contacts = []
     wb = xlrd.open_workbook(file_contents=stream.read())
     sh = wb.sheet_by_index(0)
 
     for row in range(sh.nrows):
-        defaults = {}
+        contact = {}
         for i in range(len(COLUMNS)):
             try:
                 value = sh.cell(row, i).value
-                defaults[COLUMNS[i]] = value
+                contact[COLUMNS[i]] = value
             except IndexError:
                 break
-        contact, created = create_contact(defaults, workgroups)
-        inserted += int(created)
+        contacts.append(contact)
 
-    return inserted
+    return create_contacts(contacts, 'excel', workgroups)
 
 
 def import_dispatcher(source, type_, workgroups):
