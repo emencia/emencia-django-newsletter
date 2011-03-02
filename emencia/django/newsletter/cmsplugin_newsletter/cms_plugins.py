027b402076855f79c0fbcd860ce1e09e5d703c17
Fantomas42 <fantomas42@gmail.com>
2011-03-02 16:36:44 +0100

    renaming plugin for the cms

--- emencia/django/newsletter/cmsplugin_newsletter/cms_plugins.py
+++ emencia/django/newsletter/cmsplugin_newsletter/cms_plugins.py
@@ -4,8 +4,8 @@ from django.utils.translation import ugettext_lazy as _
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
-from emencia.django.newsletter.plugins import settings
-from emencia.django.newsletter.plugins.models import SubscriptionFormPlugin
+from emencia.django.newsletter.cmsplugin_newsletter import settings
+from emencia.django.newsletter.cmsplugin_newsletter.models import SubscriptionFormPlugin
 from emencia.django.newsletter.forms import MailingListSubscriptionForm
 
 
--- emencia/django/newsletter/cmsplugin_newsletter/settings.py
+++ emencia/django/newsletter/cmsplugin_newsletter/settings.py
@@ -1,4 +1,4 @@
-"""Settings for emencia.django.newsletter.plugins"""
+"""Settings for emencia.django.newsletter.cmsplugin_newsletter"""
 from django.conf import settings
 
 FORM_NAME = getattr(settings, 'SUBSCRIPTION_FORM_NAME', 'cms_subscription_form_plugin')
