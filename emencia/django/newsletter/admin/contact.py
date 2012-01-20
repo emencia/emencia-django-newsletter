fb6fefb985888a9bc0294ad40a31cb68a5abdb1f
Fantomas42 <fantomas42@gmail.com>
2012-01-20 12:45:22 +0100

    handing gracefully error for mailing list with 999 contacts, thanks to Danols for his work and suggestion

--- emencia/django/newsletter/admin/contact.py
+++ emencia/django/newsletter/admin/contact.py
@@ -1,5 +1,6 @@
 """ModelAdmin for Contact"""
 import StringIO
+from django.conf import settings
 from datetime import datetime
 
 from django.contrib import admin
@@ -12,6 +13,7 @@ from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponseRedirect
 from django.contrib.admin.views.main import ChangeList
+from django.db import DatabaseError
 
 from emencia.django.newsletter.models import MailingList
 from emencia.django.newsletter.settings import USE_WORKGROUPS
@@ -95,7 +97,15 @@ class ContactAdmin(admin.ModelAdmin):
         new_mailing = MailingList(name=_('New mailinglist at %s') % when,
                                   description=_('New mailing list created in admin at %s') % when)
         new_mailing.save()
-        new_mailing.subscribers = queryset.all()
+
+        if 'lite' in settings.DATABASES['default']['ENGINE']:
+            self.message_user(request, _('SQLite3 or a SpatialLite database type detected, ' \
+                                         'please note you will be limited to 999 contacts ' \
+                                         'per mailing list.'))
+        try:
+            new_mailing.subscribers = queryset.all()
+        except DatabaseError:
+            new_mailing.subscribers = queryset.none()
 
         if not request.user.is_superuser and USE_WORKGROUPS:
             for workgroup in request_workgroups(request):
